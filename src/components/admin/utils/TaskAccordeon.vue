a580af31aea7e32e97aff78cb156028c8aab5b57
Mau <mauseptim@gmail.com>
2024-10-30 00:57:23 -0600

    Añadir dropdowns en VER TAREAS

---
+++ src/components/admin/utils/TaskAccordeon.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="content-container">
+    <a class="titulo" @click="toggleDropDown">
+      <div class="info-task" id="info-task">
+        <span>{{ titulo }}</span>
+        <span>{{ fechaLimite }}</span>
+      </div>
+      <img
+        src="../../../assets/drop.png"
+        alt="dropdown"
+        :class="{ rotated: isRotated }"
+      />
+    </a>
+    <div class="content" :class="{ expanded: isExpanded }">
+      <ItemTarea
+        v-for="(task, index) in tasks"
+        :task="task"
+        :options="['Pepe', 'Mau', 'Juan']"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemTarea from "./ItemTarea.vue";
+
+export default {
+  name: "TitleAccordeon",
+  components: {
+    ItemTarea
+  },
+  data() {
+    return {
+        tasks: [
+            {
+            title: '',
+            description: '',
+            date: '',
+            deadline: '' 
+        }],
+      isRotated: false,
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggleDropDown() {
+      this.isRotated = !this.isRotated;
+      this.isExpanded = !this.isExpanded;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.content-container {
+    width: 100%;
+    height: 100%;
+}
+
+.titulo {
+  display: flex;
+  position: left;
+  align-items: center;
+  text-decoration: none;
+  justify-content: space-between;
+  cursor: pointer;
+  width: 100%;
+  height: auto;
+  padding: 15px 0 15px 0;
+  border-radius: 4px;
+}
+
+.titulo h1 {
+  font-size: 1.8rem;
+  text-align: left;
+}
+
+.titulo img {
+  width: 1rem;
+  height: 1rem;
+  margin-right: 1.3rem;
+  transform: rotate(90deg);
+  transition: transform 0.3s ease;
+}
+
+.titulo img.rotated {
+  transform: rotate(0deg);
+}
+
+.titulo:hover {
+  background-color: #f2f2f2;
+}
+
+.content {
+  overflow: hidden;
+  width: 100%;
+  max-height: 0;
+  position: relative;
+  transition: max-height 650ms;
+}
+
+.content.expanded {
+  max-height: 50rem;
+  width: 100%;
+}
+</style>
---
+++ src/components/admin/utils/TitleAccordeon.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="content-container">
+    <a class="titulo" @click="toggleDropDown">
+      <h1>Tareas publicadas</h1>
+      <img
+        src="../../../assets/drop.png"
+        alt="dropdown"
+        :class="{ rotated: isRotated }"
+      />
+    </a>
+    <div class="content" :class="{ expanded: isExpanded }">
+        <TaskAccordeon />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskAccordeon from "./TaskAccordeon.vue";
+
+export default {
+  name: 'TitleAccordeon',
+  components: {
+    TaskAccordeon
+  },
+  data() {
+    return {
+      tasks: [
+        { titulo: "Tarea 1" },
+        { titulo: "Tarea 2" },
+        { titulo: "Tarea 3" },
+        { titulo: "Tarea 4" },
+      ],
+      isRotated: false,
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggleDropDown() {
+      this.isRotated = !this.isRotated;
+      this.isExpanded = !this.isExpanded;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.content-container {
+  width: 100%;
+}
+
+.titulo {
+  display: flex;
+  position: relative;
+  align-items: center;
+  text-decoration: none;
+  justify-content: space-between;
+  cursor: pointer;
+  padding: 15px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 4px;
+}
+
+.titulo h1 {
+  font-size: 1.8rem;
+  text-align: left;
+}
+
+.titulo img {
+  width: 1.8rem;
+  height: 2.7rem;
+  transition: transform 0.3s ease;
+}
+
+.titulo img.rotated {
+  transform: rotate(180deg);
+}
+
+.titulo:hover {
+  background-color: #f2f2f2;
+}
+
+.content {
+  overflow: hidden;
+  max-height: 0;
+  width: 100%;
+  height: auto;
+  position: relative;
+  transition: max-height 650ms;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+}
+.content a {
+    width: 100%;
+}
+
+.content.expanded {
+  max-height: 20rem;
+}
+</style>
